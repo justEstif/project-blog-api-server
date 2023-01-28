6d5d835c428eac312847916a7d7bbd44abe4f280
Estifanos <estifanosbeyene21@gmail.com>
2023-01-28 09:09:52 -0500

    refactor: minor formatting

--- src/app.ts
+++ src/app.ts
@@ -1,66 +1,66 @@
-import express, { Application, json, urlencoded } from 'express'
-import helmet from 'helmet'
-import compression from 'compression'
-import { connect } from 'mongoose'
-import cookieParser from 'cookie-parser'
-import endpoints from './utils/endpoints'
-import IController from './interface/controller.interface'
-import errorMiddleware from './middleware/error.middleware'
-import passportController from './authentication/passport.controller'
-import passport from 'passport'
+import express, { Application, json, urlencoded } from "express";
+import helmet from "helmet";
+import compression from "compression";
+import { connect } from "mongoose";
+import cookieParser from "cookie-parser";
+import endpoints from "./utils/endpoints";
+import IController from "./interface/controller.interface";
+import errorMiddleware from "./middleware/error.middleware";
+import passportController from "./authentication/passport.controller";
+import passport from "passport";
 
 class App {
-  public app: Application
-  public port: number
+  public app: Application;
+  public port: number;
 
   constructor(controllers: IController[]) {
-    this.app = express()
-    this.port = endpoints.PORT
-    this.connectToDB()
-    this.initializeMiddlewares()
-    this.initializeControllers(controllers)
-    this.initializeErrorHandling() // initialize last
+    this.app = express();
+    this.port = endpoints.PORT;
+    this.connectToDB();
+    this.initializeMiddlewares();
+    this.initializeControllers(controllers);
+    this.initializeErrorHandling(); // initialize last
   }
 
   private initializeMiddlewares() {
-    this.app.use(json()) // body parser
-    this.app.use(urlencoded({ extended: false }))
-    this.app.use(compression()) // compress
-    this.app.use(helmet()) // helmet
-    this.app.use(cookieParser()) // cookies
-    passportController() // setup passport
+    this.app.use(json()); // body parser
+    this.app.use(urlencoded({ extended: false }));
+    this.app.use(compression()); // compress
+    this.app.use(helmet()); // helmet
+    this.app.use(cookieParser()); // cookies
+    passportController(); // setup passport
     this.app.use(passport.initialize());
   }
 
   private initializeControllers(controllers: IController[]) {
     controllers.forEach((controller) => {
-      this.app.use(controller.router)
-    })
+      this.app.use(controller.router);
+    });
   }
 
   private initializeErrorHandling() {
-    this.app.use(errorMiddleware)
+    this.app.use(errorMiddleware);
   }
 
   private async connectToDB() {
     try {
       await connect(
         `mongodb+srv://${endpoints.MONGO_USER}:${endpoints.MONGO_PASSWORD}@${endpoints.MONGO_PATH}`
-      )
-      console.log(`MongoDB Connected`)
+      );
+      console.log(`MongoDB Connected`);
     } catch (err) {
-      console.log(`MongoDB error: ${err}`)
-      process.exit(1)
+      console.log(`MongoDB error: ${err}`);
+      process.exit(1);
     }
   }
 
   public listen() {
-    this.app.listen(this.port, () => {
+    this.app.listen(this.port, "0.0.0.0", () => {
       console.log(
         `⚡️[server]: Server is running at https://localhost:${this.port}`
-      )
-    })
+      );
+    });
   }
 }
 
-export default App
+export default App;
